c27960c3a8a86bb93c3d148c4f48f3ebfd5bd8c8
tiha97tank <tiha97tank@yahoo.co.jp>
2018-08-14 14:44:05 +0900

    ピンボールのファイルです。得点を表示させました。

---
+++ Assets/TOKUTEN.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TOKUTEN : MonoBehaviour
+{
+
+
+    public GameObject Tokuten;
+    private int score = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        this.Tokuten = GameObject.Find("tokutenText");
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.Tokuten.GetComponent<Text>().text = "スコア " + score.ToString();
+
+
+    }
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            score += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            score += 15;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            score += 5;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 15;
+        }
+
+    }
+}
